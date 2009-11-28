f34a2fcba6249615cde412f03eff3d4e8033ba0b
Wojciech Pietrzak <wojciech@wojciech-laptop.(none)>
2009-11-28 23:31:43 +0100

    globalize options translated_attributes

--- lib/active_record/globalize_accessors.rb
+++ lib/active_record/globalize_accessors.rb
@@ -6,24 +6,21 @@ module ActiveRecord
 
     module ActMethods
       def globalize_accessors(*attr_names)
-        options = attr_names.extract_options!
-        options[:easy_accessors] = attr_names
-        
-        options[:easy_accessors].each do |attr_name|
-          options[:locales].each do |with_locale|
+        languages = attr_names
+        attribs = globalize_options[:translated_attributes]
+        attribs.each do |attr_name|
+          languages.each do |with_locale|
             define_method :"#{attr_name}_#{with_locale}" do
               globalize.fetch with_locale, attr_name
             end
-            
+
             define_method :"#{attr_name}_#{with_locale}=" do |val|
               globalize.stash with_locale, attr_name, val
               self[attr_name] = val
             end
           end
         end
-        
-        
-      end
+
     end
   end
 end
