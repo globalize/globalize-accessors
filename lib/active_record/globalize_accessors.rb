e34f9bae3cc4fe0799e2f40269e525f1f3e93aec
Steve Verlinden <steve.verlinden@gmail.com>
2010-03-13 18:26:05 +0100

    fix temp attributes values

--- lib/active_record/globalize_accessors.rb
+++ lib/active_record/globalize_accessors.rb
@@ -11,15 +11,27 @@ module ActiveRecord
         attribs.each do |attr_name|
           languages.each do |with_locale|
             define_method :"#{attr_name}_#{with_locale}" do
-              globalize.fetch with_locale, attr_name
+              if @temp_attributes and @temp_attributes[with_locale][attr_name]
+                @temp_attributes[with_locale][attr_name]
+              else
+                globalize.fetch with_locale, attr_name
+              end
             end
 
             define_method :"#{attr_name}_#{with_locale}=" do |val|
+              initialize_temp_attributes unless @temp_attributes
+              @temp_attributes[with_locale][attr_name] = val
               globalize.stash.write with_locale, attr_name, val
               self[attr_name] = val
             end
           end
-        end  
+        end
+         
+        define_method :initialize_temp_attributes do
+          @temp_attributes = Hash[*languages.collect { |v| [v, {}]}.flatten]
+        end
+        
+        after_save :initialize_temp_attributes 
       end
     end
   end
