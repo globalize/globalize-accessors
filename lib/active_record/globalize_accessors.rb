cfecb4054ba52b9bbbd124adb03491a7a641a343
Robert Pankowecki <robert.pankowecki@gmail.com>
2010-08-16 23:04:41 +0800

    Fix indentation

--- lib/active_record/globalize_accessors.rb
+++ lib/active_record/globalize_accessors.rb
@@ -1,32 +1,32 @@
 module ActiveRecord
   module GlobalizeAccessors
-      def globalize_accessors(*attr_names)
-        languages = attr_names
-        attribs = translated_attribute_names
-        attribs.each do |attr_name|
-          languages.each do |with_locale|
-            define_method :"#{attr_name}_#{with_locale}" do
-              if @temp_attributes and @temp_attributes[with_locale][attr_name]
-                @temp_attributes[with_locale][attr_name]
-              else
-                globalize.fetch with_locale, attr_name
-              end
-            end
-
-            define_method :"#{attr_name}_#{with_locale}=" do |val|
-              initialize_temp_attributes unless @temp_attributes
-              @temp_attributes[with_locale][attr_name] = val
-              globalize.stash.write with_locale, attr_name, val
-              self[attr_name] = val
+    def globalize_accessors(*attr_names)
+      languages = attr_names
+      attribs = translated_attribute_names
+      attribs.each do |attr_name|
+        languages.each do |with_locale|
+          define_method :"#{attr_name}_#{with_locale}" do
+            if @temp_attributes and @temp_attributes[with_locale][attr_name]
+              @temp_attributes[with_locale][attr_name]
+            else
+              globalize.fetch with_locale, attr_name
             end
           end
+
+          define_method :"#{attr_name}_#{with_locale}=" do |val|
+            initialize_temp_attributes unless @temp_attributes
+            @temp_attributes[with_locale][attr_name] = val
+            globalize.stash.write with_locale, attr_name, val
+            self[attr_name] = val
+          end
         end
+      end
          
-        define_method :initialize_temp_attributes do
-          @temp_attributes = Hash[*languages.collect { |v| [v, {}]}.flatten]
-        end
+      define_method :initialize_temp_attributes do
+        @temp_attributes = Hash[*languages.collect { |v| [v, {}]}.flatten]
+      end
         
-        after_save :initialize_temp_attributes
+      after_save :initialize_temp_attributes
     end
   end
 end
